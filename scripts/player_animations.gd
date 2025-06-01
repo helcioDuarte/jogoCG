2ee0082177480ef175dcf9c8605b29a480d7109c
ravsil <rafaelvieiradas@gmail.com>
2025-06-01 00:33:02 -0300

    added basic walking animations

---
+++ scripts/player_animations.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@onready var animTree = $AnimationTree
+@onready var animStateMachine = $AnimationTree.get('parameters/playback')
+var currentFrame = 0.0
+
+func animateMovement(velocity, speed):
+	if getState() == "idle|walk":
+		currentFrame = lerp(currentFrame, velocity.length() / speed, 0.1)
+		animTree.set("parameters/idle|walk/blend_position", currentFrame)
+	elif getState() == "walk|run":
+		currentFrame = lerp(currentFrame, (velocity.length() / speed * 2) - 1, 0.1)
+		animTree.set("parameters/walk|run/blend_position", currentFrame)
+
+func changeWalkRun(goal):
+	if goal == "run":
+		animStateMachine.travel("walk|run")
+		if Input.is_action_just_pressed("sprint"):
+			currentFrame = -1.0
+	elif goal == "walk":
+		animStateMachine.travel("idle|walk")
+
+func getState():
+	return animStateMachine.get_current_node()
